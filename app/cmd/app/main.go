246cdeac620b7259d1967c35c53f5991dd022685
bogdan demchenko <bogdandemchenko@gmail.com>
2022-05-07 00:47:50 +0300

    added dependencies

--- app/cmd/app/main.go
+++ app/cmd/app/main.go
@@ -1,5 +1,18 @@
 package app
 
-func main() {
+import (
+	"log"
+	"production_service/internal/config"
+)
 
+func main() {
+	log.Print("config initializing")
+	cfg := config.GetConfig()
+
+	log.Print("logger initializing")
+
+	app, err := app.NewApp(cfg)
+	if err != nil {
+
+	}
 }
